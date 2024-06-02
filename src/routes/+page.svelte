636b5c8191e876793dfefd1e4b26e1892deb0d91
John Aldrich Bernardo <4ldrich@protonmail.com>
2024-06-02 22:41:00 +0800

    :rocket: initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Banner from "$lib/ui/banner/component.svelte";
+</script>
+
+<h1 class="text-red-900 underline m-3">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Banner />
