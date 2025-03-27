53b5bdf29bfa5931c9b206760d2ef43b8435fe6a
John Aldrich Bernardo <4ldrich@protonmail.com>
2025-03-28 00:34:16 +0800

    :construction: optimizations

--- src/lib/ui/card/component.svelte
+++ src/lib/ui/card/component.svelte
@@ -7,7 +7,7 @@
 
 <a
 	href={link}
-	class="mb-4 mr-4 flex min-h-[218px] flex-col rounded-lg border-[12px] border-t-[6px] border-b-[#adb19b] border-l-[#d2d6cd] border-r-[#caceb8] border-t-[#dce2d4] bg-[#edebe5] p-2 pl-2 shadow-[7px_7px_3px_#c9b7b7] transition duration-100 active:shadow-[0_0_4px_#494746] md:min-h-[145px]"
+	class="mb-4 mr-4 flex min-h-[218px] flex-col rounded-lg border-[12px] border-t-[6px] border-b-[#adb19b] border-l-[#d2d6cd] border-r-[#caceb8] border-t-[#dce2d4] bg-[#edebe5] p-2 pl-2 shadow-[7px_7px_3px_#c9b7b7] transition duration-100 active:shadow-[0_0_4px_#494746] md:min-h-[145px] outline-none"
 >
 	<div class="flex flex-row">
 		{#if icon}
--- src/lib/utils/cloudinary.ts
+++ src/lib/utils/cloudinary.ts
@@ -1,4 +1,5 @@
 import { Cloudinary, type CloudinaryImage } from "@cloudinary/url-gen";
+import { ResizeSimpleAction } from "@cloudinary/url-gen/actions/resize/ResizeSimpleAction";
 
 const cld = new Cloudinary({
   cloud: {
@@ -7,6 +8,6 @@ const cld = new Cloudinary({
 });
 
 export const getCloudinaryImage = (publicId: string): CloudinaryImage => {
-  return cld.image(publicId);
+  return cld.image(publicId).quality("auto:eco").resize(new ResizeSimpleAction("fill", 248));
 };
 
